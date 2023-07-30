52d67f10488aa80f232ac9607a452fa44e7b9066
awaisnazir08 <owaisnazir2228@gmail.com>
2023-07-31 01:49:09 +0500

    props done about to enter the states

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar'
 
 function App() {
   return (
     <>
-    <Navbar/>
+    <Navbar title="Text Utils" aboutText="About TextUtils"/>
     </>
   );
 }
