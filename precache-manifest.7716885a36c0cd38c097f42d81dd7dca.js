480125aa9218e46821a84391c74a1d74baac14a1
awaisnazir08 <owaisnazir2228@gmail.com>
2023-08-17 19:51:10 +0500

    Updates

---
+++ precache-manifest.7716885a36c0cd38c097f42d81dd7dca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "54161933905c2e25da93",
+    "url": "/React-Course/static/css/main.19eed0bc.chunk.css"
+  },
+  {
+    "revision": "54161933905c2e25da93",
+    "url": "/React-Course/static/js/main.f77d5914.chunk.js"
+  },
+  {
+    "revision": "c974c7fedb130063eeb6",
+    "url": "/React-Course/static/js/runtime~main.8de9a30e.js"
+  },
+  {
+    "revision": "f3afda117f246c896c6c",
+    "url": "/React-Course/static/js/2.ca48165b.chunk.js"
+  },
+  {
+    "revision": "bd2db5e315108ebec595",
+    "url": "/React-Course/static/js/3.5b0ae9b5.chunk.js"
+  },
+  {
+    "revision": "36d53a1edef8856fe3152be7959e8ed8",
+    "url": "/React-Course/index.html"
+  }
+];
